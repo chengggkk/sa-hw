cd2aa470d9121d9e475d5a0e2ebd76b227337dc6
chengggkk <chengggkk@gmail.com>
2024-04-01 11:29:43 +0800

    Add moment and react-dates dependencies, implement DateRangePicker component

--- src/App.js
+++ src/App.js
@@ -1,22 +1,39 @@
 import logo from './logo.svg';
 import './App.css';
+import 'react-dates/initialize';
+import { DateRangePicker, SingleDatePicker, DayPickerRangeController } from 'react-dates';
+import 'react-dates/lib/css/_datepicker.css';
+import React, { useState } from 'react';
+
+export const pureComponentAvailable = true;
 
 function App() {
+  const [startDate, setStartDate] = useState(null);
+  const [endDate, setEndDate] = useState(null);
+  const [focusedInput, setFocusedInput] = useState(null);
+
+  const handleDatesChange = ({ startDate, endDate }) => {
+    setStartDate(startDate);
+    setEndDate(endDate);
+  };
+
+  const handleFocusChange = (focusedInput) => {
+    setFocusedInput(focusedInput);
+  };
+
   return (
     <div className="App">
       <header className="App-header">
-        {/* <img src={logo} className="App-logo" alt="logo" /> */}
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+
+        <DateRangePicker
+          startDate={startDate}
+          startDateId="your_unique_start_date_id"
+          endDate={endDate}
+          endDateId="your_unique_end_date_id"
+          onDatesChange={handleDatesChange}
+          focusedInput={focusedInput}
+          onFocusChange={handleFocusChange}
+        />
       </header>
     </div>
   );
